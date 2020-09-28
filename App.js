d430e92a92bfe74bde843a2491b7f3e3cf40a9bb
lus <alexander.lysenko.dev@gmail.com>
2020-09-29 00:58:52 +0300

    Rework existed codebase

--- App.js
+++ App.js
@@ -1,50 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import {
   Button, Text,
-  TextInput, View
+  TextInput, View, StyleSheet
 } from 'react-native';
 
 
-export default class aTommy extends Component {
+export default function main() {
+  // const [videoUrl, setVideoUrl] = useState("")
+  const [metadata, setMetadata] = useState("")
 
-  state = {
-    url: "",
-    video: "Response"
-  };
+  return (
+    <View styles={containerStyle}>
+      <Text>Video URL:</Text>
+      <TextInput styles={inputFieldStyle}
+        onSubmitEditing={changeVideoUrl} />
+      <Text>{JSON.stringify({ metadata })}</Text>
+    </View>
+  );
 
-  changeVideoUrl = (view) => {
+  function changeVideoUrl(view) {
     const text = view.nativeEvent.text;
     console.log(text);
-    this.setState(state => {
-      this.fetchVideo(text);
-    });
-  };
-
-  render() {
-    return (<View
-      style={{ margin: 10 }}>
-
-      <Text>Video URL:</Text>
-      <TextInput
-        style={{
-          height: 35,
-          borderColor: 'gray',
-          borderWidth: 1
-        }}
-        onSubmitEditing = {value => this.changeVideoUrl(value)} />
-      <Text>{JSON.stringify(this.state.video)}</Text>
-    </View>
-   );
+    fetchVideo(text);
   }
 
-  fetchVideo = (url) => {
-    return (fetch(url)
-    .then(response => response.json())
-    .then(json => { 
-      console.log(json);
-      this.setState({ url: url, video: json });
-    })
-    .catch(error => { console.log(error) }));
-  };
+  function fetchVideo(url) {
+    return (
+      fetch(url)
+        .then(response => response.json())
+        .then(json => {
+          console.log(json);
+          // setVideoUrl(url)
+          setMetadata(json)
+        })
+        .catch(error => { console.log(error) })
+    );
+  }
 
 }
+
+const containerStyle = StyleSheet.create({
+  margin: 10
+})
+
+const inputFieldStyle = StyleSheet.create({
+  height: 35,
+  borderColor: 'gray',
+  borderWidth: 1
+})
